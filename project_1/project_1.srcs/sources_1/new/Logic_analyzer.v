7e222a9c288389cc3c947193b11e9b00c6128f54
DennisvanKrieken <DennisvanKrieken@gmail.com>
2026-03-06 17:55:40 +0100

    INIT

---
+++ project_1/project_1.srcs/sources_1/new/Logic_analyzer.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: None
+// Engineer: Dennis
+// 
+// Create Date: 06.03.2026 17:53:18
+// Design Name: Logic_analyzer
+// Module Name: Logic_analyzer
+// Project Name: Logic_analyzer
+// Target Devices: PYNQ-Z2
+// Tool Versions: 
+// Description: A n-bit logic analyzer that outputs to ethernet.
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Logic_analyzer(
+    input clk,
+    input reset,
+    output [3:0] leds
+    );
+endmodule
